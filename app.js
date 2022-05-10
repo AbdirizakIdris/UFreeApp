c4c6bf4736a9f8231ea3bf502effa8831affdf49
Michalina Klupiec <michalina@Michalinas-MacBook-Air.local>
2022-05-10 16:08:21 +0100

    log-in and test

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const methodOverride = require("method-override");
 const cookieParser = require("cookie-parser");
 const path = require("path");
 
+
 const homeRouter = require("./routes/home");
 const usersRouter = require("./routes/users"); 
+const sessionsRouter = require("./routes/sessions")
 
 const app = express()
 
@@ -34,4 +36,7 @@ app.use(
 
 // route setup
 app.use("/users", usersRouter);
+app.use("/sessions", sessionsRouter)
+// app.use("/", homeRouter)
+
 module.exports = app;
