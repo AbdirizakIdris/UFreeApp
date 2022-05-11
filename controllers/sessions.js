bcd41fab04334a2c477d45e77cfdfd5ff7f5b58e
jkgiwa <jkgiwa95@gmail.com>
2022-05-11 13:02:33 +0100

    edited sessions/new.hbs and edited user_can_log_in_spec.js

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -10,10 +10,17 @@ const SessionsController = {
         const email = req.body.email;
         const password = req.body.password;
 
+      
       User.findOne({ email: email }).then((user) => {
+        if (!user) {
+          res.redirect("/sessions/new#bademail");
+        } else if (user.password !== password) {
+          res.redirect("/sessions/new#badpassword");
+        } else {
           req.session.user = user;
           res.redirect("/users/personal-page");
-        });
+        }
+      });
     },
 }     
 
