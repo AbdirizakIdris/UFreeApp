b812ffabfa6e51f87ea4e9e10483c6536b1eb5de
Michalina Klupiec <michalina@Michalinas-MacBook-Air.local>
2022-05-11 11:37:15 +0100

    log in test

--- routes/sessions.js
+++ routes/sessions.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const SessionsController = require("../controllers/sessions");
 
 router.get("/new", SessionsController.New);
+router.post("/", SessionsController.Create);
 
 module.exports = router;
